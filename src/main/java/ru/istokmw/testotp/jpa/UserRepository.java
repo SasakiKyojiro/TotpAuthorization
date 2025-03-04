ba3f97ad418279782254e8f00b33ddfc5ce53097
gorbun <pythonjavaproger@gmail.com>
2025-03-04 17:12:56 +0300

    fix - Подключил БД

--- src/main/java/ru/istokmw/testotp/jpa/UserRepository.java
+++ src/main/java/ru/istokmw/testotp/jpa/UserRepository.java
@@ -2,11 +2,13 @@ package ru.istokmw.testotp.jpa;
 
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends R2dbcRepository<Member, UUID> {
     @Query("SELECT * FROM auth.member Where 'name'=:username")
     Mono<Member> findByName(String username);
